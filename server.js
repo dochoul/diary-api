f9bb02067a39da33a042e0d5be165c830af8a615
dochoul@gmail.com <dochoul@gmail.com>
2023-12-13 21:04:23 +0900

    init

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const PORT = 9000;
+const mongoose = require("mongoose");
+const cors = require("cors");
+const router = require("./routes/book-routes");
+
+//* Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router);
+
+//* DB Connect
+mongoose
+  .connect(
+    "mongodb+srv://admin:1234@cluster0.4ffeuqe.mongodb.net/bookStore?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB Connection Successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("from get route");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on${PORT}`);
+});
+
+//* User Schema
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     require: true,
+//   },
+// });
+// const User = mongoose.model("User", userSchema);
+
+// app.post("/createuser", async (req, res) => {
+//   try {
+//     const bodyData = req.body;
+//     const user = new User(bodyData);
+//     const userData = await user.save();
+//     res.send(userData);
+//     console.log("ok?");
+//   } catch (error) {
+//     res.send(error);
+//   }
+// });
