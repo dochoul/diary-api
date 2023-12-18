187ac436164e734258e998a97f56884d8c37e5e6
dochoul@gmail.com <dochoul@gmail.com>
2023-12-18 20:29:31 +0900

    불필요한 컬럼 삭제

--- model/Book.js
+++ model/Book.js
@@ -3,34 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  name: {
-    type: String,
-    require: true,
-  },
-  author: {
-    type: String,
-    require: true,
-  },
-  description: {
-    type: String,
-    require: true,
-  },
-  price: {
-    type: Number,
-    require: true,
-  },
-  available: {
-    type: Boolean,
-    require: true,
-  },
-  image: {
-    type: String,
-    require: true,
-  },
-  today: {
-    type: Date,
-    require: true,
-  },
   emotion: {
     type: Number,
     require: true,
@@ -39,21 +11,9 @@ const bookSchema = new Schema({
     type: String,
     require: true,
   },
-  contents: {
-    type: String,
-    require: true,
-  },
   date: {
     type: Date,
     require: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
   },
 });
 
