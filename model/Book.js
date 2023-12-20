62cc304767bc0210ab949d371ad82242c499fa25
dochoul@gmail.com <dochoul@gmail.com>
2023-12-20 15:08:09 +0900

    ,{timestamps:true}

--- model/Book.js
+++ model/Book.js
@@ -2,19 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-  emotion: {
-    type: Number,
-    require: true,
+const bookSchema = new Schema(
+  {
+    emotion: {
+      type: Number,
+      require: true,
+    },
+    content: {
+      type: String,
+      require: true,
+    },
+    date: {
+      type: Date,
+      require: true,
+    },
   },
-  content: {
-    type: String,
-    require: true,
-  },
-  date: {
-    type: Date,
-    require: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Book", bookSchema);
