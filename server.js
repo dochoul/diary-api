13abd5e17c2482c48074f960c84d81b0dd5fb684
dochoul@gmail.com <dochoul@gmail.com>
2023-12-23 01:12:41 +0900

    cors

--- server.js
+++ server.js
@@ -15,33 +15,6 @@ app.use(
 );
 app.use("/books", router);
 
-var whitelist = [
-  "https://my-emotion-diary.netlify.app/",
-  "http://localhost:9000",
-]; //white list consumers
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-    "device-remember-token",
-    "Access-Control-Allow-Origin",
-    "Origin",
-    "Accept",
-  ],
-};
-app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
-
 //* DB Connect
 mongoose
   .connect(
