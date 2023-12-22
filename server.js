4fab9e979ff7c04eaec1bb6fef47077fdd1a2f57
dochoul@gmail.com <dochoul@gmail.com>
2023-12-23 01:12:12 +0900

    cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 //app.use(cors());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://tasteless-bianka-david-kim.koyeb.app/",
   })
 );
 app.use("/books", router);
